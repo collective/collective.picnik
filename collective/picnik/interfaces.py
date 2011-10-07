db5d9fef0fb1cecd0873096508512e7e7fbee295
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-10-07 13:37:55 +0200

    fix plone4.1 compatibility

--- collective/picnik/interfaces.py
+++ collective/picnik/interfaces.py
@@ -3,12 +3,16 @@ from zope import interface
 
 #dependencies
 try:
-    #plone4
-    from Products.ATContentTypes.interfaces.image import IATImage as IImage
+    from Products.ATContentTypes.interfaces import IATImage as IImage
 except ImportError, e:
-    logger.info('BBB: switch to plone3 %s'%e)
-    #plone3
-    from Products.ATContentTypes.interface import IATImage  as IImage
+    try:
+        #plone4
+        from Products.ATContentTypes.interfaces.image import IATImage as IImage
+        logger.info('BBB: switch to plone4.0 %s'%e)
+    except ImportError, e:
+        logger.info('BBB: switch to plone3 %s'%e)
+        #plone3
+        from Products.ATContentTypes.interface import IATImage  as IImage
 
 class IPicnikLayer(interface.Interface):
     """Browser layer for picnik addon"""
